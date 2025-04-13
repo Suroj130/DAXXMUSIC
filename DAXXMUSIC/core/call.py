8c425dd772f90de13ce164c5ea29ac5b90ad2d9b
Suroj130 <sandipbera.ind@gmail.com>
2025-04-13 22:33:08 +0530

    Update call.py

--- DAXXMUSIC/core/call.py
+++ DAXXMUSIC/core/call.py
@@ -20,7 +20,7 @@ from pytgcalls.types import (
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from BrandrdXMusic import LOGGER, YouTube, app
+from DAXXMUSIC import LOGGER, YouTube, app
 from DAXXMUSIC.misc import db
 from DAXXMUSIC.utils.database import (
     add_active_chat,
